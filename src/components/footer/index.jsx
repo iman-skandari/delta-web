4a27b9cdb434c39993174bd3ddf4e324653c6d23
iman-skandari <iimisk9919@gmail.com>
2023-07-01 16:17:33 +0330

    create footerup

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./footer.css";
+
+export default () => {
+  return (
+    <div>
+      <div className="footerUp">
+        <a> درباره ما</a>
+        <a>تماس با ما</a>
+        <a>طراحی وب سایت ملکی</a>
+        <a> فرصت های شغلی</a>
+        <a>تبلیغات در دلتا</a>
+        <a>دلتا مگ</a>
+      </div>
+
+      <div className="footerMidle"></div>
+
+      <div className="footerDown"></div>
+    </div>
+  );
+};
