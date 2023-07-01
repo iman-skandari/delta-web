c343ce6286bffdbc33898d135de93a69b1d247a3
iman-skandari <iimisk9919@gmail.com>
2023-07-01 13:38:24 +0330

    create navbarup

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./navbar.css";
+import arrowimg from "../../assets/images.PNG";
 
-export default () => {
-
-    var name = "ali"
-
+export default ({ img }) => {
   return (
-    <div>
-        <div>
-            ssdsdsds
+    <div className="navbarWraoer">
+      <div className="navbarUp">
+        <div className="navbarUpRight">
+          <img src={img} alt="" />
+          <a>بازار املاک</a>
         </div>
-    </div>
-  )
-}
+        <div className="navbarUpLeft">
+          <div className="navbarUpLeft1">
+            <p>تهران</p>
+            <img src={arrowimg} alt="" />
+          </div>
+          <div className="navbarUpLeft2">
+            <p>ورود به حساب کاربری</p>
+            <p>|</p>
+            <p className="navbarUpLeftTitle">همکاری با مشاور/آژانس املاک</p>
+          </div>
+        </div>
+      </div>
+      <div className="navbarDown">
 
+      </div>
+    </div>
+  );
+};
